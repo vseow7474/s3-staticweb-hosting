0d147dcc137484d6b3cbf7e8b144aa66e5bdc7ec
jaezeu <jazeel.meerasah@gmail.com>
2024-07-05 18:05:47 +0800

    add comments

--- main.tf
+++ main.tf
@@ -28,13 +28,13 @@ resource "aws_s3_bucket_website_configuration" "website" {
 }
 
 resource "aws_route53_record" "www" {
-  zone_id = data.aws_route53_zone.sctp_zone.zone_id
-  name    = "jazeelstaticwebsite"
+  zone_id = data.aws_route53_zone.sctp_zone.zone_id  #Zone ID of hosted zone: sctp-sandbox.com
+  name    = "jazeelstaticwebsite"                    # Bucket name prefix, before your domain
   type    = "A"
 
   alias {
-    name                   = aws_s3_bucket_website_configuration.website.website_domain
-    zone_id                = aws_s3_bucket.static_bucket.hosted_zone_id
+    name                   = aws_s3_bucket_website_configuration.website.website_domain  #S3 website configuration attribute: website_domain
+    zone_id                = aws_s3_bucket.static_bucket.hosted_zone_id                  # Hosted zone of the S3 bucket, Attribute: hosted_zone_id
     evaluate_target_health = true
   }
 }
